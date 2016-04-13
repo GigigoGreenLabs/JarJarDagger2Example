87b21c560f5127ee585b4de4b1becbe8646e2746
Yohan Hartanto <yhartanto@twitter.com>
2016-04-13 00:09:12 -0700

    Move everything to shadow and use project dependencies, instead of file based lib files.

--- library/src/main/java/ox/com/daggertest/TestModule.java
+++ library/src/main/java/ox/com/daggertest/TestModule.java
@@ -1,15 +1,17 @@
 package ox.com.daggertest;
 
-import javax.inject.Singleton;
 
-import dagger.Module;
-import dagger.Provides;
+import javax.inject.Singleton2;
 
-@Module
+import dagger.Module2;
+import dagger.Provides2;
+
+
+@Module2
 public class TestModule {
 
-    @Singleton
-    @Provides
+    @Singleton2
+    @Provides2
     LoggerApp provideLogger() {
         return new LoggerApp();
     }
